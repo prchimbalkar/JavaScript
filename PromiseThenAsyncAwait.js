6f580b05325eca51ac0498920276007b68d64eb5
Chimbalkar Pankaj <chimbalkar.pankaj@bcg.com>
2020-04-04 11:27:48 +0530

    Promise-then async-await

---
+++ PromiseThenAsyncAwait.js
@@ -0,0 +1,35 @@
+//function returning promise
+const fnPromise = (flag) => {
+	return new Promise((resolve, reject) => {
+		if (flag) {
+			resolve('true');
+		} else {
+			reject('flase');
+		}
+	});
+}
+
+//promise then
+fnPromise(true).then((result) => {
+	console.log(result);
+});
+
+//async await
+(async() => {
+	let result = await fnPromise(true);
+	console.log(result);
+})();
+
+//async function
+const fnResolver = async() => {
+let result = await fnPromise(true);
+	console.log(result);
+}
+//call: fnResolver();
+
+//with conventional approach
+async function fnResolverConventional() {
+let result = await fnPromise(true);
+	console.log(result);
+}
+//call: fnResolverConventional();
