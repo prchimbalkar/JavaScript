e8631346b86554235dc8ea0841283d8e7eb4a32d
Chimbalkar Pankaj <chimbalkar.pankaj@bcg.com>
2020-04-04 10:45:03 +0530

    Function returning function

---
+++ FunctionReturningFunction.js
@@ -0,0 +1,14 @@
+const fnParent = (() => {
+
+	//Member functions as properties of parent function
+	const fnPrintStr = (str) => { console.log(str);};
+	const fnPrintHelloStr = (str) => { console.log('Hello' + str);};
+	const fnAlertStr = (str) => { alert(str);};
+
+	//Return multiple functions from one function
+	return {
+		fnPrint: fnPrintStr,
+		fnHello: fnPrintHelloStr,
+		fnAlert: fnAlertStr
+		};
+})();
